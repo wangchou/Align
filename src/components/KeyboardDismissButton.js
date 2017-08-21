5cbf5b56a3b5e8f1aa515b0e87a8e3e592ddb607
Wangchou Lu <wangchou.lu@honestbee.com>
2017-08-21 16:16:44 +0800

    refactor and add FloatButton component

--- src/components/KeyboardDismissButton.js
+++ src/components/KeyboardDismissButton.js
@@ -1,52 +1,17 @@
 import React, { Component } from 'react';
-import {
-  TouchableHighlight,
-  Keyboard,
-  StyleSheet,
-  View,
-  Text
-} from 'react-native';
+import FloatButton from './FloatButton';
+import { Keyboard } from 'react-native';
 
 export default class KeyboardDimissButton extends Component {
   render() {
-    const styles = StyleSheet.create({
-      button: {
-        backgroundColor: '#ff5722',
-        borderColor: '#ff5722',
-        borderWidth: 1,
-        height: 40,
-        width: 40,
-        borderRadius: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'absolute',
-        bottom: this.props.keyboardHeight + 10,
-        right:10,
-        shadowColor: "#000000",
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        shadowOffset: {
-          height: 1,
-          width: 0
-        }
-      },
-      text: {
-        fontSize: 20,
-        color: 'white'
-      }
-    });
     return (
-      <View>
-        <TouchableHighlight
-          style={styles.button}
-          underlayColor='#ff7043'
-
-          // Event Handler
-          onPress={Keyboard.dismiss}
-        >
-          <Text style={styles.text}>v</Text>
-        </TouchableHighlight>
-      </View>
+      <FloatButton
+        text={'v'}
+        color={'#ff5722'}
+        underColor={'#ff7043'}
+        onPress={Keyboard.dismiss}
+        bottom={this.props.keyboardHeight + 10}
+      />
     );
   }
 }
--- src/components/TodayButton.js
+++ src/components/TodayButton.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
-import {
-  TouchableHighlight,
-  Keyboard,
-  StyleSheet,
-  View,
-  Text
-} from 'react-native';
+import FloatButton from './FloatButton';
 import {connect} from 'react-redux';
 import {gotoTodayPage} from '../actions/books';
 
@@ -15,44 +9,14 @@ import {gotoTodayPage} from '../actions/books';
 export default class TodayButton extends Component {
   render() {
     return (
-      <View>
-        <TouchableHighlight
-          style={styles.button}
-          underlayColor='darkorange'
-
-          // Event Handler
-          onPress={this.props.gotoTodayPage}
-        >
-          <Text style={styles.text}>今</Text>
-        </TouchableHighlight>
-      </View>
+      <FloatButton
+        text={'今'}
+        color={'orange'}
+        underColor={'darkorange'}
+        onPress={this.props.gotoTodayPage}
+        bottom={10}
+      />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: 'orange',
-    borderColor: 'orange',
-    borderWidth: 1,
-    height: 40,
-    width: 40,
-    borderRadius: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
-    bottom: 10,
-    right:10,
-    shadowColor: "#000000",
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-    shadowOffset: {
-      height: 1,
-      width: 0
-    }
-  },
-  text: {
-    fontSize: 20,
-    color: 'white'
-  }
-});

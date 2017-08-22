254fb31a85c1df45f4a4387aa78f63293efe1875
Wangchou Lu <wangchou.lu@honestbee.com>
2017-08-22 14:38:12 +0800

    remove KeyboardDismissButton and add FloatEditBar

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import {
   Dimensions,
 } from 'react-native';
 import BookSwipeView from './components/BookSwipeView';
-import KeyboardDimissButton from './components/KeyboardDismissButton';
+import FloatEditBar from './components/FloatEditBar';
 import TodayButton from './components/TodayButton';
 import KeyboardManager from './components/KeyboardManager';
 import {
@@ -67,11 +67,11 @@ export default class OnigiriNote extends Component {
     );
 
     let keyboardAvoidingView = null;
-    let keyboardDismissButton = null;
+    let floatEditBar = null;
     let todayButton = null;
     if (isKeyboardShow) {
       keyboardAvoidingView = <View style={{height: keyboardHeight}} />;
-      keyboardDismissButton = <KeyboardDimissButton keyboardHeight={keyboardHeight}/>;
+      floatEditBar = <FloatEditBar keyboardHeight={keyboardHeight}/>;
     } else {
       todayButton = <TodayButton />;
     }
@@ -92,7 +92,7 @@ export default class OnigiriNote extends Component {
           <StatusBar hidden />
           {keyboardAvoidingView}
         </ScrollView>
-        {keyboardDismissButton}
+        {floatEditBar}
         {todayButton}
         <KeyboardManager verticalScrollTo={this.verticalScrollTo}/>
       </View>

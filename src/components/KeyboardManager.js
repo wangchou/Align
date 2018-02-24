a1f736e0836f598ec77cc887addac51505e49125
Wangchou Lu <kindalu@gmail.com>
2018-02-25 01:35:46 +0800

    show editbar after keyboard showed

--- src/components/KeyboardManager.js
+++ src/components/KeyboardManager.js
@@ -19,7 +19,7 @@ export default class KeyboardManager extends Component {
   componentDidMount() {
     this.props.setIsKeyboardShow(false)
 
-    this.keyboardWillShowListerner = Keyboard.addListener('keyboardWillShow', (event) => {
+    this.keyboardDidShowListerner = Keyboard.addListener('keyboardDidShow', (event) => {
       this.props.setKeyboardHeight(event.endCoordinates.height + floatEditBarHeight)
       this.props.setIsKeyboardShow(true)
     })
@@ -33,7 +33,7 @@ export default class KeyboardManager extends Component {
 
   componentWillUnmount() {
     this.keyboardWillHideListener.remove()
-    this.keyboardWillShowListener.remove()
+    this.keyboardDidShowListener.remove()
   }
 
   render() {
